cd15ffdc5ab854435e2299d179d5fab54f027f2a
Glass Hammer <hu54088971@gmail.com>
2021-02-12 22:48:08 +0800

    -implemanted scattered circle brush

--- ImpressionistDoc.cpp
+++ ImpressionistDoc.cpp
@@ -17,6 +17,7 @@
 #include "LineBrush.h"
 #include "CircleBrush.h"
 #include "ScatteredPointBrush.h"
+#include "ScatteredCircleBrush.h"
 
 
 #define DESTROY(p)	{  if ((p)!=NULL) {delete [] p; p=NULL; } }
@@ -47,7 +48,7 @@ ImpressionistDoc::ImpressionistDoc()
 	ImpBrush::c_pBrushes[BRUSH_SCATTERED_LINES]		
 		= new PointBrush( this, "Scattered Lines" );
 	ImpBrush::c_pBrushes[BRUSH_SCATTERED_CIRCLES]	
-		= new PointBrush( this, "Scattered Circles" );
+		= new ScatteredCircleBrush( this, "Scattered Circles" );
 
 	// make one of the brushes current
 	m_pCurrentBrush	= ImpBrush::c_pBrushes[0];
